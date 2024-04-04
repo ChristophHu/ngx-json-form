cfd2fa47b16c29bb8831e29041e01c6f70a627d2
Christoph Hu <christoph.huschenhoefer@gmx.de>
2024-04-04 13:41:44 +0200

    update galerie

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -139,7 +139,7 @@ export class AppComponent {
           },
           {
             type: 'select',
-            disabled: false,
+            disabled: true,
             hidden: false,
             key: 'funktion',
             label: 'Funktion',
